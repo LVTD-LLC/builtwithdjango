528b43f004802253201df4b7f5f42b87c2adadea
Rasul Kireev <me@rasulkireev.com>
2026-05-26 14:35:59 +0200

    refactor(projects): remove tweet generation

--- projects/admin.py
+++ projects/admin.py
@@ -1,9 +1,6 @@
-import asyncio
-
 from django.contrib import admin, messages
 
 from .models import Comment, Like, Project, Technology
-from .utils import tweet_project
 
 
 class CommentInline(admin.TabularInline):
@@ -12,27 +9,6 @@ class CommentInline(admin.TabularInline):
 
 class LikeInline(admin.TabularInline):
     model = Like
-
-
-def tweet_selected_projects(modeladmin, request, queryset):
-    """Admin action to tweet about selected projects."""
-    tweeted = 0
-    errors = 0
-    for project in queryset:
-        try:
-            # Run the async tweet_project function
-            asyncio.run(tweet_project(project.id))
-            tweeted += 1
-        except Exception as e:
-            errors += 1
-            modeladmin.message_user(request, f"Error tweeting about {project.title}: {e}", level=messages.ERROR)
-    if tweeted:
-        modeladmin.message_user(request, f"Successfully tweeted about {tweeted} project(s).", level=messages.SUCCESS)
-    if errors and not tweeted:
-        modeladmin.message_user(request, f"Failed to tweet about {errors} project(s).", level=messages.ERROR)
-
-
-tweet_selected_projects.short_description = "Tweet about selected project(s) on Twitter"
 
 
 def unpublish_selected_projects(modeladmin, request, queryset):
